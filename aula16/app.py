c9bd7fb33a2813601219071bbec6ead6116b1b37
dmuneratto <danielmuneratto@gmail.com>
2022-02-10 16:37:07 -0300

    updates

--- aula16/app.py
+++ aula16/app.py
@@ -36,6 +36,12 @@ db = SQLAlchemy(app)
 #     def __repr__(self):
 #         return '<Plate %r>' % self.name
 
+class Client(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(50))
+    phone = db.Column(db.String(14))
+    orders = db.relationship('Orders', back_populates='client', lazy=True)
 
 
 class Plate(db.Model, SerializerMixin):
@@ -72,10 +78,16 @@ class Category(db.Model, SerializerMixin):
 class Orders(db.Model, SerializerMixin):
     serialize_rules = ('-plate.orders','-plate.plate.orders','-plate.order_id', '-plate.id')
     id = db.Column(db.Integer, primary_key=True)
+    
     code = db.Column(db.Integer, nullable=False, unique=True)
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+    client_id = db.Column(db.Integer, db.ForeignKey(
+    'client.id', ondelete="CASCADE"), nullable=False)
+
+    client = db.relationship(
+    'Client', back_populates='orders')
     plate = db.relationship("PlateOrder", backref="orders")
 
     def __repr__(self):
